2e7f174bf4b64ca8a847e382246d79b44be0d629
Fafurin <ffafurin@mail.ru>
2022-05-27 23:35:49 +0300

    Updates

---
+++ precache-manifest.bd077445300e0f4d71b1562ef4d7918f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a57bfe60283346a758af",
+    "url": "/static/css/main.d2457952.chunk.css"
+  },
+  {
+    "revision": "a57bfe60283346a758af",
+    "url": "/static/js/main.a57bfe60.chunk.js"
+  },
+  {
+    "revision": "4d3952047f8b866a6584",
+    "url": "/static/js/1.4d395204.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1d48e38aceb096c9d6a58ad9bff25287",
+    "url": "/index.html"
+  }
+];
